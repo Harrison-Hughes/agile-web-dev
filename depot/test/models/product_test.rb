1c5d1ec56553810cb514acdcc0d555eef617090a
github username <harr.hughes@gmail.com>
2020-10-10 19:46:03 +0100

    product test/fixtures written

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 # product test class
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -28,7 +29,41 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 0
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+    
     product.price = 1
     assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      price: 1,
+      image_url: image_url
+    )
+  end
+
+  test "image url valid" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+      http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+    end 
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+      description: "yyy",
+      price: 1,
+      image_url: "fred.gif"
+    )
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
 end 
