97a2a99a8c53b6572390f3f6a6b38fe6a27c2c88
github username <harr.hughes@gmail.com>
2020-10-11 13:37:41 +0100

    line_item scaffold created

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :carts
   root 'store#index', as: 'store_index'
   resources :products
+  resources :carts
+  resources :line_items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
