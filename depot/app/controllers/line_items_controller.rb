9fc0e5c94b05107460b2f81afc727b669e2b5bbf
github username <harr.hughes@gmail.com>
2020-10-12 19:36:56 +0100

    up to remove from basket button

--- depot/app/controllers/line_items_controller.rb
+++ depot/app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart, only: [:create]
+  before_action :set_cart, only: [:create, :destroy]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -26,6 +26,11 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
+    print ''' 
+
+      CREATE METHOD CALLED
+
+    '''
     product = Product.find(params[:product_id]) 
     @line_item = @cart.add_product(product)
 
@@ -57,11 +62,32 @@ class LineItemsController < ApplicationController
 
   # DELETE /line_items/1
   # DELETE /line_items/1.json
+  # def destroy
+  #   @line_item.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+  
   def destroy
-    @line_item.destroy
+    print ''' 
+
+      DESTROY METHOD CALLED
+      
+    '''
+    product = Product.find(params[:product_id])
+    @line_item = @cart.minus_one_product(product)
+
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
-      format.json { head :no_content }
+      if !!@line_item 
+        format.html { redirect_to store_index_url}
+        format.js { @current_item = @line_item }
+        format.json { head :no_content }
+      else 
+        format.html { redirect_to store_index_url, notice: 'Line item was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
   end
 
--- depot/app/models/cart.rb
+++ depot/app/models/cart.rb
@@ -11,6 +11,16 @@ class Cart < ApplicationRecord
     current_item
   end
 
+  def minus_one_product(product)
+    current_item = line_items.find_by(product_id: product.id)
+    if current_item.quantity > 1
+      current_item.quantity -= 1
+    else 
+      current_item.destroy
+    end
+    current_item
+  end
+
   def total_price
     line_items.to_a.sum { |item| item.total_price}
   end
